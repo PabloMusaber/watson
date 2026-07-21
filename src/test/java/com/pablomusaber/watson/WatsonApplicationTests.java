9500ac306cc76eb6e4bb24c8710d0690b76f1475
PabloMusaber <musaberpablo@gmail.com>
2026-07-20 21:28:48 -0300

    initial commit

---
+++ src/test/java/com/pablomusaber/watson/WatsonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pablomusaber.watson;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WatsonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
